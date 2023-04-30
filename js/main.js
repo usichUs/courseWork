47c6b4f7f2b520556fcbfcbe7c83e367bad25ec0
Nikita Usachev <guyfoks737@gmail.com>
2023-04-30 22:02:41 +0300

    hope this one will be last

--- js/main.js
+++ js/main.js
@@ -1,27 +1,20 @@
-let 
-
-for (let li of tree.querySelectorAll('li')) {
-    let span = document.createElement('span');
-    span.classList.add('show');
-    li.prepend(span);
-    span.append(span.nextSibling);
-}
-
-tree.onclick = function (event) {
-    if (event.target.tagName != 'SPAN') return;
-
-    let childrenContainer = event.target.parentNode.querySelector('ul');
-
-    if (!childrenContainer) return;
-
-    childrenContainer.hidden = !childrenContainer.hidden;
-
-    if (childrenContainer.hidden) {
-        event.target.classList.add('hide');
-        event.target.classList.remove('show');
+class File {
+    constructor(name, size, date) {
+      this.name = name;
+      this.size = size;
+      this.date = date;
     }
-    else {
-        event.target.classList.add('show');
-        event.target.classList.remove('hide');
+  }
+  
+  class Folder {
+    constructor(name, files, folders) {
+      this.name = name;
+      this.files = files;
+      this.folders = folders;
     }
-}
+  }
+
+  const folder = new Folder("Каталог 1", [
+    new File("Файл 1.1", 1024, new Date()),
+    new File("Файл 1.2", 2048, new Date())
+  ], []);
