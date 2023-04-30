78ab41801dda6340ae746e7b7ba4e25ccf062984
Nikita Usachev <guyfoks737@gmail.com>
2023-04-30 19:39:41 +0300

    started

---
+++ js/main.js
@@ -0,0 +1,25 @@
+for (let li of tree.querySelectorAll('li')) {
+    let span = document.createElement('span');
+    span.classList.add('show');
+    li.prepend(span);
+    span.append(span.nextSibling);
+}
+
+tree.onclick = function (event) {
+    if (event.target.tagName != 'SPAN') return;
+
+    let childrenContainer = event.target.parentNode.querySelector('ul');
+
+    if (!childrenContainer) return;
+
+    childrenContainer.hidden = !childrenContainer.hidden;
+
+    if (childrenContainer.hidden) {
+        event.target.classList.add('hide');
+        event.target.classList.remove('show');
+    }
+    else {
+        event.target.classList.add('show');
+        event.target.classList.remove('hide');
+    }
+}
